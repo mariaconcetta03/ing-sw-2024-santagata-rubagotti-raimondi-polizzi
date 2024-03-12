2ce2432b9cc4c1d2ba7e73c81cc137404d981ede
Andrea <andrea.rubagotti2002@gmail.com>
2024-03-12 22:54:24 +0100

    Prova Andrea IntelliJ: 22.54

--- PROGETTO/src/main/java/org/example/App.java
+++ PROGETTO/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Codex Naturalis! Prova Andre 2. Sono Francesco" );
+     System.out.println( "Sono Andrea" );
     }
 }
