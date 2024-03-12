b72b26b2affb2b349e89956bef06c8d0acd4eebf
franc <francesco.raimondi3@mail.polimi.it>
2024-03-12 22:29:19 +0100

    provo di nuovo

--- PROGETTO/src/main/java/org/example/App.java
+++ PROGETTO/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Codex Naturalis! Prova Andre 2" );
+        System.out.println( "Codex Naturalis! Prova Andre 2. Sono Fra" );
     }
 }
