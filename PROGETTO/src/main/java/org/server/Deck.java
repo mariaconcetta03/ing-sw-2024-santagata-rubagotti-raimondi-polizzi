dcead549f003df968bfa3946ff24d7b8917069f5
Andrea <andrea.rubagotti2002@gmail.com>
2024-03-16 18:05:17 +0100

    Implemented classes: Deck

--- PROGETTO/src/main/java/org/server/Deck.java
+++ PROGETTO/src/main/java/org/server/Deck.java
@@ -21,3 +21,26 @@ public class Deck {
     }
 
 }
+package org.server;
+import java.util.*;
+
+public class Deck {
+    private Stack<Integer> cards;
+
+    public Deck() {
+        cards = new Stack<>();
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(cards);
+    }
+
+    public int getFirstCard() {
+        return cards.pop().getId();
+    }
+
+    public boolean isFinished() {
+        return cards.isEmpty(); //if it's finished, then returns TRUE
+    }
+
+}
