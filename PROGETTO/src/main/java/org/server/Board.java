412fd8a761b0b450e5be943b112d6bfc7f7cfef2
Chiara <chiara.polizzi@mail.polimi.it>
2024-03-16 13:36:06 +0100

    modifications to Board

--- PROGETTO/src/main/java/org/server/Board.java
+++ PROGETTO/src/main/java/org/server/Board.java
@@ -1,22 +1,60 @@
 package org.server;
 
-import java.util.List;
-import java.util.Set;
+import jdk.internal.loader.Resource;
+
+import java.util.*;
 
 public class Board {
     private Set<Coordinates> playablePositions;
     private Set<Coordinates> unPlayablePositions;
-    private int[][] table;
+    private Map<Integer,Map<Integer,Integer>> table;
     private int maxExtensionX;
     private int minExtensionX;
     private int maxExtensionY;
     private int minExtensionY;
     private List<Integer> playedCards;
     private int[] numResourcesPlaced;
-    public void addPlayablePositions(int id){
+    public void addPlayable&UnplayablePositions(Integer id){ //una volta che tramite controller il giocatore sceglie il posto (viene inizializzato attributo position)
+        Card c=getCardById(id);
+        if(c.orientation) {
+            if (c.get_front_up_right().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findUpRight());
+            } else if (!(c.get_front_up_right().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findUpRight());
+            } else if (c.get_front_up_left().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findUpLeft());
+            }else if (!(c.get_front_up_left().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findUpLeft());
+            } else if (c.get_front_down_right().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findDownRight());
+            } else if (!(c.get_front_down_right().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findDownRight());
+            } else if (c.get_front_down_left().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findDownLeft());
+            }else if (!(c.get_front_down_left().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findDownLeft());
+            }
+        }else if(!(c.orientation)) {
+            if (c.get_back_up_right().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findUpRight());
+            } else if (!(c.get_back_up_right().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findUpRight());
+            } else if (c.get_back_up_left().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findUpLeft());
+            }else if (!(c.get_back_up_left().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findUpLeft());
+            } else if (c.get_back_down_right().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findDownRight());
+            } else if (!(c.get_back_down_right().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findDownRight());
+            } else if (c.get_back_down_left().equals(ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.position.findDownLeft());
+            }else if (!(c.get_back_down_left().equals(ResourceType.ABSENT))) {
+                playablePositions.add(c.position.findDownLeft());
+            }
 
-    }
-    public void addUnplayablePositions(int id){
+        }
+
 
     }
     public void checkPlayablePositions(){
@@ -59,5 +97,18 @@ public class Board {
 
     public void setMinExtensionY(int minExtensionY) {
         this.minExtensionY = minExtensionY;
+    }
+    public void placeCard(Integer id){ //assumo che nel controller ci sia un metodo per permettere al giocatore di scegliere dove mettere la carta
+        int x=getCardById(id).position.getX();
+        int y=getCardById(id).position.getY();
+        if(!(table.containsKey(x))){
+            table.put(x, new HashMap<>());
+        }
+        table.get(x).put(y,id);
+
+    }
+    public void placeFirstCard(Integer id) {
+        table.put(0,new HashMap<>()); //prima abbiamo table completamente vuoto
+        table.get(0).put(0,id);
     }
 }
