d14e5774feeedd4cf3a9efdc64822cc59ba5fa83
franc <francesco.raimondi3@mail.polimi.it>
2024-03-16 20:55:03 +0100

    codex 1.3

--- PROGETTO/src/main/java/org/server/Codex.java
+++ PROGETTO/src/main/java/org/server/Codex.java
@@ -20,6 +20,10 @@ public class Codex {
     //that is also the one deciding how many players the game will have (2-3-4)
     public void startLobby(Player creator){
         Game newGame= new Game();
+        System.out.println("How many players to play this game? ");
+        int nPlayers= sc.nextInt();
+        //mi serve un setter per settare il numero di giocatori
+        newGame.setPlayers(nPlayers);
 
 
     }
@@ -41,16 +45,34 @@ public class Codex {
         if(!allGames.isEmpty()) {
             System.out.println("Already started games are:");
             for (int i = 0; i < allGames.size(); i++) {
-                System.out.print(allGames.get(i) + "   ");
+                if (allGames.get(i).getState()== Game.GameState.WAITING_FOR_START) {
+                    System.out.print(allGames.get(i) + "   ");
+                }
             }
         }else{
             startLobby(newPlayer);
+            return;
         }
-        System.out.println("Would you like to join an ");
-        for(int i=0; i<allGames.size();i++){
+        //hp the user gives me good answer, not stupid ones (we could later implement a check-system)
+        System.out.println("Type 0 if you want to create a new game or the code of an already started" +
+                "game if you want to join it: ");
+        String decision=sc.nextLine();
+        if(decision.equals("1")){
+            startLobby(newPlayer);
+        }else{
+            Game tmp;
+            for(int i=0; i<allGames.size();i++){
+                if(allGames.get(i).getCode().equals(decision)){
+                    tmp=allGames.get(i);
+                    break;
+                }
+            }
+            //mi serve un getter da Game per la lista Players
+            tmp.getPlayers().add(newPlayer);
+            //it will be duty of the Game object to understand if the lobby is full and so
+            // change its state to STARTED
+        }
 
-        }
-        //have to understand how to connect to games
     }
     public void disconnectPlayer(){
 
