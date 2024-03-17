4422fe81224ba7fb170ca0168e3290bc4e71581e
Andrea <andrea.rubagotti2002@gmail.com>
2024-03-17 13:13:16 +0100

    Modified classes: Objective_Card

--- PROGETTO/src/main/java/org/server/Objective_Card.java
+++ PROGETTO/src/main/java/org/server/Objective_Card.java
@@ -1,9 +1,8 @@
 package org.server;
 
-public class Objective_Card extends Card{
-        private int cardPoints;
-        private int pointsToPlayer;
-
+public class Objective_Card extends Card {
+        private int cardPoints; //points of the card
+        private int pointsToPlayer; //points to add to the player (for example if the objective is achieved more times)
         public Objective_Card (int points) {
             this.cardPoints = points;
         }
