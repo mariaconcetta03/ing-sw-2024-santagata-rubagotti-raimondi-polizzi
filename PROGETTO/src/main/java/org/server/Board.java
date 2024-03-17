312aa19490d84f355194476f1f31056cd2a7a605
Chiara <chiara.polizzi@mail.polimi.it>
2024-03-17 13:10:06 +0100

    adjustments to Board

--- PROGETTO/src/main/java/org/server/Board.java
+++ PROGETTO/src/main/java/org/server/Board.java
@@ -14,45 +14,45 @@ public class Board {
     private int maxExtensionY;
     private int minExtensionY;
     private List<Integer> playedCards;
-    private Map<Coordinates,ResourceType> externalResourcesPositions;
-    private Map<ResourceType, Integer> numberOfExternalAndCentralResources;
+    private Map<Coordinates, Playable_Card.ResourceType> externalResourcesPositions;
+    private Map<Playable_Card.ResourceType, Integer> numberOfExternalAndCentralResources;
     public void addPlayablePlusUnplayablePositions(Integer id){ //una volta che tramite controller il giocatore sceglie il posto (viene inizializzato attributo position)
-        Card c=getCardById(id);
-        if(c.orientation) {
-            if (c.get_front_up_right().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findUpRight());
-            } else if (!(c.get_front_up_right().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findUpRight());
-            } else if (c.get_front_up_left().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findUpLeft());
-            }else if (!(c.get_front_up_left().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findUpLeft());
-            } else if (c.get_front_down_right().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findDownRight());
-            } else if (!(c.get_front_down_right().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findDownRight());
-            } else if (c.get_front_down_left().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findDownLeft());
-            }else if (!(c.get_front_down_left().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findDownLeft());
+        Playable_Card c=Codex.getInstance().getCardById(id);
+        if(c.getOrientation()) {
+            if (c.get_front_up_right().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findUpRight());
+            } else if (!(c.get_front_up_right().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findUpRight());
+            } else if (c.get_front_up_left().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findUpLeft());
+            }else if (!(c.get_front_up_left().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findUpLeft());
+            } else if (c.get_front_down_right().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findDownRight());
+            } else if (!(c.get_front_down_right().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findDownRight());
+            } else if (c.get_front_down_left().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findDownLeft());
+            }else if (!(c.get_front_down_left().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findDownLeft());
             }
-        }else if(!(c.orientation)) {
-            if (c.get_back_up_right().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findUpRight());
-            } else if (!(c.get_back_up_right().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findUpRight());
-            } else if (c.get_back_up_left().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findUpLeft());
-            }else if (!(c.get_back_up_left().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findUpLeft());
-            } else if (c.get_back_down_right().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findDownRight());
-            } else if (!(c.get_back_down_right().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findDownRight());
-            } else if (c.get_back_down_left().equals(ResourceType.ABSENT)) {
-                unPlayablePositions.add(c.position.findDownLeft());
-            }else if (!(c.get_back_down_left().equals(ResourceType.ABSENT))) {
-                playablePositions.add(c.position.findDownLeft());
+        }else if(!(c.getOrientation())) {
+            if (c.get_back_up_right().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findUpRight());
+            } else if (!(c.get_back_up_right().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findUpRight());
+            } else if (c.get_back_up_left().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findUpLeft());
+            }else if (!(c.get_back_up_left().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findUpLeft());
+            } else if (c.get_back_down_right().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findDownRight());
+            } else if (!(c.get_back_down_right().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findDownRight());
+            } else if (c.get_back_down_left().equals(Playable_Card.ResourceType.ABSENT)) {
+                unPlayablePositions.add(c.getPosition().findDownLeft());
+            }else if (!(c.get_back_down_left().equals(Playable_Card.ResourceType.ABSENT))) {
+                playablePositions.add(c.getPosition().findDownLeft());
             }
 
         }
@@ -101,35 +101,36 @@ public class Board {
         this.minExtensionY = minExtensionY;
     }
     public void placeCard(Integer id){ //assumo che nel controller ci sia un metodo per permettere al giocatore di scegliere dove mettere la carta
-        int x=getCardById(id).position.getX();
-        int y=getCardById(id).position.getY();
+        Playable_Card tmp = (Playable_Card) Codex.getInstance().getCardById(id);
+        int x=tmp.getPosition().getX();
+        int y=tmp.getPosition().getY();
         if(!(table.containsKey(x))){
             table.put(x, new HashMap<>());
         }
         table.get(x).put(y,id);
         //sottraggo la risorsa coperta
-        numberOfExternalAndCentralResources.get(externalResourcesPositions.get(getCardById(id).position))--;
-        externalResourcesPositions.remove(getCardById(id).position); //tolgo l'angolo occupato
+        numberOfExternalAndCentralResources.get(externalResourcesPositions.get(tmp.getPosition()))--;
+        externalResourcesPositions.remove(tmp.getPosition()); //tolgo l'angolo occupato
         //aggiungo le nuove risorse (4 angoli)
-        if(!(getCardById(id).orientation)) {
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_up_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findUpRight(),getCardById(id).get_back_up_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_up_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.finfUpLeft(),getCardById(id).get_back_up_left ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_down_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownRight(),getCardById(id).get_back_down_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_down_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownLeft(),getCardById(id).get_back_down_left ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_center())++; //abbiamo assunto che la risorsa in centro è solo da un lato
-        }else if(getCardById(id).orientation){
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_up_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findUpRight(),getCardById(id).get_front_up_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_up_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.finfUpLeft(),getCardById(id).get_front_up_left ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_down_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownRight(),getCardById(id).get_front_down_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_down_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownLeft(),getCardById(id).get_front_down_left ());
+        if(!(tmp.getOrientation())) {
+            numberOfExternalAndCentralResources.get(tmp.get_back_up_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findUpRight(),tmp.get_back_up_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_up_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().finfUpLeft(),tmp.get_back_up_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_down_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownRight(),tmp.get_back_down_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_down_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownLeft(),tmp.get_back_down_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_center())++; //abbiamo assunto che la risorsa in centro è solo da un lato
+        }else if(tmp.getOrientation()){
+            numberOfExternalAndCentralResources.get(tmp.get_front_up_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findUpRight(),tmp.get_front_up_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_up_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().finfUpLeft(),tmp.get_front_up_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_down_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownRight(),tmp.get_front_down_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_down_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownLeft(),tmp.get_front_down_left ());
         }
 
 
@@ -140,43 +141,44 @@ public class Board {
     public void placeFirstCard(Integer id) {
         table.put(0,new HashMap<>()); //prima abbiamo table completamente vuoto
         table.get(0).put(0,id); //la prima carta non copre nessun angolo
+        Playable_Card tmp = (Playable_Card) Codex.getInstance().getCardById(id);
         //le carte iniziali sono le uniche che possono avere più di una risorsa al centro
         //devo aggiungere le risorse al centro
-        if(!(getCardById(id).getOrientation())){ //retro sempre con simbolo in centro
-            if(getCardById(id).get_back_center().equals(ResourceType.NATURE_FUNGI)) {
-                numberOfExternalAndCentralResources.get(NATURE)++;
-                numberOfExternalAndCentralResources.get(FUNGI)++;
-            }else if(getCardById(id).get_back_center().equals(ResourceType.ANIMAL_INSECT)){
-                numberOfExternalAndCentralResources.get(ANIMAL)++;
-                numberOfExternalAndCentralResources.get(INSECT)++;
-            }else if(getCardById(id).get_back_center().equals(ResourceType.ANIMAL_INSECT_NATURE)){
-                numberOfExternalAndCentralResources.get(ANIMAL)++;
-                numberOfExternalAndCentralResources.get(INSECT)++;
-                numberOfExternalAndCentralResources.get(NATURE)++;
-            }else if(getCardById(id).get_back_center().equals(ResourceType.NATURE_ANIMAL_FUNGI)){
-                numberOfExternalAndCentralResources.get(NATURE)++;
-                numberOfExternalAndCentralResources.get(ANIMAL)++;
-                numberOfExternalAndCentralResources.get(FUNGI)++;
+        if(!(tmp.getOrientation())){ //retro sempre con simbolo in centro
+            if(tmp.get_back_center().equals(Playable_Card.ResourceType.NATURE_FUNGI)) {
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.NATURE)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.FUNGI)++;
+            }else if(tmp.get_back_center().equals(Playable_Card.ResourceType.ANIMAL_INSECT)){
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.ANIMAL)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.INSECT)++;
+            }else if(tmp.get_back_center().equals(Playable_Card.ResourceType.ANIMAL_INSECT_NATURE)){
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.ANIMAL)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.INSECT)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.NATURE)++;
+            }else if(tmp.get_back_center().equals(Playable_Card.ResourceType.NATURE_ANIMAL_FUNGI)){
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.NATURE)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.ANIMAL)++;
+                numberOfExternalAndCentralResources.get(Playable_Card.ResourceType.FUNGI)++;
             }else{
-                numberOfExternalAndCentralResources.get(getCardById(id).get_back_center())++;
+                numberOfExternalAndCentralResources.get(tmp.get_back_center())++;
             }
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_up_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findUpRight(),getCardById(id).get_back_up_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_up_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.finfUpLeft(),getCardById(id).get_back_up_left ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_down_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownRight(),getCardById(id).get_back_down_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_back_down_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownLeft(),getCardById(id).get_back_down_left ());
-        }else if(getCardById(id).getOrientation()){ //non ho risorse al centro
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_up_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findUpRight(),getCardById(id).get_front_up_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_up_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.finfUpLeft(),getCardById(id).get_front_up_left ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_down_right ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownRight(),getCardById(id).get_front_down_right ());
-            numberOfExternalAndCentralResources.get(getCardById(id).get_front_down_left ())++;
-            externalResourcesPositions.put(getCardById(id).position.findDownLeft(),getCardById(id).get_front_down_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_up_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findUpRight(),tmp.get_back_up_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_up_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().finfUpLeft(),tmp.get_back_up_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_down_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownRight(),tmp.get_back_down_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_back_down_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownLeft(),tmp.get_back_down_left ());
+        }else if(tmp.getOrientation()){ //non ho risorse al centro
+            numberOfExternalAndCentralResources.get(tmp.get_front_up_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findUpRight(),tmp.get_front_up_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_up_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().finfUpLeft(),tmp.get_front_up_left ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_down_right ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownRight(),tmp.get_front_down_right ());
+            numberOfExternalAndCentralResources.get(tmp.get_front_down_left ())++;
+            externalResourcesPositions.put(tmp.getPosition().findDownLeft(),tmp.get_front_down_left ());
         }
     }
     public void addToListANewPlayedCard(Integer id){
