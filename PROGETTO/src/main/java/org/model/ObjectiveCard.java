924c3e96f3d38030f14ede76a9ab723d2de44cda
franc <francesco.raimondi3@mail.polimi.it>
2024-03-20 20:17:53 +0100

    try commits on Objective_card_number

--- PROGETTO/src/main/java/org/model/ObjectiveCard.java
+++ PROGETTO/src/main/java/org/model/ObjectiveCard.java
@@ -5,24 +5,43 @@ public class ObjectiveCard extends Card {
         private int pointsToPlayer; // points to add to the player (for example if the objective is achieved more times)
                                     // example: the card gives you 2 points for each couple of NATURE. If p1 has
                                     // 5 natures, then pointsToPlayer will be 4 (2 x 2)
+
+        /**
+         * Class constructor
+         * @param points
+         */
         public ObjectiveCard(int points) {
             this.cardPoints = points;
         }
 
+        /**
+         * updates player's points
+         * @param points
+         */
         public void addPointsToPlayer(int points) {
             this.pointsToPlayer = this.pointsToPlayer + points;
         }
 
+        /**
+         * Getter
+         * @return cardPoints
+         */
         public int getCardPoints(){
-            return this.cardPoints;
-        }
+                return this.cardPoints;
+            }
 
+        /**
+         * Getter
+          * @return pointsToPlayer
+         */
         public int getPointsToPlayer() {
-            return this.pointsToPlayer;
-        }
+                return this.pointsToPlayer;
+            }
 
-        public void resetPointsToPlayer() { // PointsToPlayer is set to 0, for example to begin the
-                                            // counting of points for another different player
-            this.pointsToPlayer = 0;
-        }
+        /**
+         * PointsToPlayer is set to 0, for example to begin the counting of points for another different player
+         */
+        public void resetPointsToPlayer() {
+                this.pointsToPlayer = 0;
+            }
 }
