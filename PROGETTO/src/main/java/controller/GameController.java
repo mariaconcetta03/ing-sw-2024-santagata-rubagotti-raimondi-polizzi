8b91a98e84ca6afce9a2b6cdfbdca7fc001d93e7
franc <francesco.raimondi3@mail.polimi.it>
2024-03-24 23:05:52 +0100

    GameController first ideas

--- PROGETTO/src/main/java/controller/GameController.java
+++ PROGETTO/src/main/java/controller/GameController.java
@@ -1,22 +1,58 @@
 package controller;
+import org.model.Coordinates;
 import org.model.Game;
 import org.model.PlayableCard;
+import org.model.Player;
 
 import java.util.*;
 public class GameController {
-    public Game startGame(){
+    private Game game;
 
+    /**
+     *
+     */
+    public void startGame(){
+        game.setState(Game.GameState.WAITING_FOR_START);
+        game.startGame();
     }
-    public void playCard(){
 
+    /**
+     * This method "plays" the card selected by the Player in his own Board
+     * @param selectedCard the Card the Player wants to play
+     * @param position the position where the Player wants to play the Card
+     * @param orientation the side on which the Player wants to play the Card
+     * @return true if the card was correctly played, false otherwise
+     */
+    public boolean playCard(PlayableCard selectedCard, Coordinates position, boolean orientation) {
+        Player p1 = game.getPlayers().get(0);
+        try {
+            p1.playCard(selectedCard, position, orientation);
+            return true;
+        }catch(IllegalArgumentException e){
+            return false;
+        }
     }
-    public PlayableCard drawCard(){
 
+    /**
+     * This method allows the currentPlayer to draw a card from the decks or from the unveiled ones
+     * @param selectedCard is the Card the Players wants to draw
+     */
+    public void drawCard(PlayableCard selectedCard){
+        Player currentPlayer= game.getPlayers().get(0);
+        currentPlayer.drawCard(selectedCard);
     }
+
+    /**
+     *
+     */
     public void nextPhase(){
 
     }
-    public void endGame(){
 
+    /**
+     *
+     */
+    public void endGame(){
+        game.setState(Game.GameState.ENDED);
     }
 }
