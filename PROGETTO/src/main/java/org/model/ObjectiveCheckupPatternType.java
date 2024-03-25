06e59f9e05c1af632dcb5e9b79062a119a5a07ee
Chiara <chiara.polizzi@mail.polimi.it>
2024-03-25 08:35:47 +0100

    modifications to ObjectiveCheckupPatternType to write the idea

--- PROGETTO/src/main/java/org/model/ObjectiveCheckupPatternType.java
+++ PROGETTO/src/main/java/org/model/ObjectiveCheckupPatternType.java
@@ -1,7 +1,16 @@
 package org.model;
 
+import java.util.Map;
+
 public class ObjectiveCheckupPatternType implements ObjectiveCheckup{
     public int countObjectiveCardRealPoints(Player player){
+        //after we gather our variables and instruments...:
+        Map<Coordinates,Card> copy; //assegnazione con qualcosa creata in Board nell'evenienza qualcuno abbiamo pescato un objectiveCardPattern
+        int counter=0;
+        for(Coordinates k : copy.keySet()){ //that is to have an idea of what we ca do
+            if(copy.get(k+objectiveCard.getPositionCard1())==getCard1Color()){
 
+            }
+        }
     }
 }
