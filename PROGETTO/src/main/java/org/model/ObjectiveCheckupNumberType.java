78326f56768536923fd2694e2047699a74c91c9f
Chiara <chiara.polizzi@mail.polimi.it>
2024-03-25 08:23:56 +0100

    adding of getters and setters in Objective_card_pattern plus some comments

--- PROGETTO/src/main/java/org/model/ObjectiveCheckupNumberType.java
+++ PROGETTO/src/main/java/org/model/ObjectiveCheckupNumberType.java
@@ -1,6 +1,11 @@
 package org.model;
 
 public class ObjectiveCheckupNumberType implements ObjectiveCheckup {
+    /**
+     * here we can add some methods in Objective_card_number used to count the points
+     * @param player
+     * @return
+     */
     public int countObjectiveCardRealPoints(Player player){
 
     }
--- PROGETTO/src/main/java/org/model/Objective_card_pattern.java
+++ PROGETTO/src/main/java/org/model/Objective_card_pattern.java
@@ -15,4 +15,44 @@ public class Objective_card_pattern extends ObjectiveCard{
     public Objective_card_pattern(int points, objectiveCardType type) {
         super(points, type);
     }
+
+    public Coordinates getPositionCard1() {
+        return positionCard1;
+    }
+
+    public void setPositionCard1(Coordinates positionCard1) { //these are relative coordinates maybe initialized by Jason
+        this.positionCard1 = positionCard1;
+    }
+
+    public Coordinates getPositionCard2() {
+        return positionCard2;
+    }
+
+    public void setPositionCard2(Coordinates positionCard2) {
+        this.positionCard2 = positionCard2;
+    }
+
+    public Pawn getCard0Color() {
+        return card0Color;
+    }
+
+    public void setCard0Color(Pawn card0Color) {
+        this.card0Color = card0Color;
+    }
+
+    public Pawn getCard1Color() {
+        return card1Color;
+    }
+
+    public void setCard1Color(Pawn card1Color) {
+        this.card1Color = card1Color;
+    }
+
+    public Pawn getCard2Color() {
+        return card2Color;
+    }
+
+    public void setCard2Color(Pawn card2Color) {
+        this.card2Color = card2Color;
+    }
 }
