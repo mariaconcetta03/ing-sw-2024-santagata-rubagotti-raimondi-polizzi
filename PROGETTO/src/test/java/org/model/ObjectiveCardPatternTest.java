2634e91c2e88867c20321ac0e4cf3fb0112f0f92
Chiara <chiara.polizzi@mail.polimi.it>
2024-04-06 19:06:57 +0200

    usage of assertEquals function in ObjectiveCardPatternTest

--- PROGETTO/src/test/java/org/model/ObjectiveCardPatternTest.java
+++ PROGETTO/src/test/java/org/model/ObjectiveCardPatternTest.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals; //inspired by Nardi's code :)
 
 public class ObjectiveCardPatternTest extends TestCase {
     public void addPointsToPlayerTest(){
         //let's use for example the blue diagonal pattern (down to up, left to right)
-        Objective_card_pattern usedCard = new Objective_card_pattern(2,new Coordinates(1,-1),new Coordinates(2,-2),AngleType.INSECT,AngleType.INSECT,AngleType.INSECT);
+        Objective_card_pattern usedCard = new Objective_card_pattern(2,new Coordinates(1,1),new Coordinates(2,2),AngleType.INSECT,AngleType.INSECT,AngleType.INSECT);
         Map<Coordinates,AngleType> playedCards=new HashMap<>();
         playedCards.put(new Coordinates(1,1),AngleType.INSECT);
-        assertEquals();
+        playedCards.put(new Coordinates(2,2),AngleType.INSECT);
+        playedCards.put(new Coordinates(3,3),AngleType.INSECT);
+        assertEquals(usedCard.addPointsToPlayer(new Player()),2); //the algorithm is correct if it gives 2 points to the player
 
     }
 }
