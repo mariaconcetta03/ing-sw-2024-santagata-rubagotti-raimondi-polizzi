72abf083b80a995935b63905567b5e6927d959b2
Andrea <andrea.rubagotti2002@gmail.com>
2024-04-09 15:04:42 +0200

    ObjectiveCard: deleted unused methods

--- PROGETTO/src/main/java/org/model/ObjectiveCard.java
+++ PROGETTO/src/main/java/org/model/ObjectiveCard.java
@@ -13,9 +13,6 @@ public class ObjectiveCard extends Card {
         private Player player;
         //private objectiveCardType type;
         private int cardPoints; // points the card gives when the player achieves the goal
-        //private int pointsToPlayer; // points to add to the player (for example if the objective is achieved more times)
-                                    // example: the card gives you 2 points for each couple of NATURE. If p1 has
-                                    // 5 natures, then pointsToPlayer will be 4 (2 x 2)
 
 
         /**
@@ -40,25 +37,5 @@ public class ObjectiveCard extends Card {
         public int getCardPoints(){
                 return this.cardPoints;
             }
-
-        /**
-         * Getter
-          * @return pointsToPlayer
-
-        public int getPointsToPlayer() {
-                return this.pointsToPlayer;
-            }
-
-
-
-        /**
-         * PointsToPlayer is set to 0, for example to begin the counting of points for another different player
-
-        public void resetPointsToPlayer() { //we have first to give the points to the right player
-                this.pointsToPlayer = 0;
-        }
-
-        */
-
 
 }
