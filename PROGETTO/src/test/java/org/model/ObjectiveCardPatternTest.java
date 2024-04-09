9a29ceba7189d6c1171ae3b8074a64215ad170c9
MariaConcettaSantagata <mariaconcetta.santagata@mail.polimi.it>
2024-04-09 22:28:32 +0200

    ObjectiveCardPatternTest completed

--- PROGETTO/src/test/java/org/model/ObjectiveCardPatternTest.java
+++ PROGETTO/src/test/java/org/model/ObjectiveCardPatternTest.java
@@ -17,16 +17,6 @@ public class ObjectiveCardPatternTest extends TestCase {
         playedCards.put(new Coordinates(2,2),AngleType.INSECT);
         playedCards.put(new Coordinates(3,3),AngleType.INSECT);
         assertEquals(usedCard.addPointsToPlayer(new Player()),2); //the algorithm is correct if it gives 2 points to the player
-
     }
-    public void baseTest(){
-        //let's use for example the blue diagonal pattern (down to up, left to right)
-        Objective_card_pattern usedCard = new Objective_card_pattern(2,new Coordinates(1,1),new Coordinates(2,2),AngleType.INSECT,AngleType.INSECT,AngleType.INSECT);
-        Map<Coordinates,AngleType> playedCards=new HashMap<>();
-        playedCards.put(new Coordinates(1,1),AngleType.INSECT);
-        playedCards.put(new Coordinates(2,2),AngleType.INSECT);
-        playedCards.put(new Coordinates(3,3),AngleType.INSECT);
-        assertEquals(usedCard.addPointsToPlayer(new Player()),2); //the algorithm is correct if it gives 2 points to the player
-
-    }
+   
 }
