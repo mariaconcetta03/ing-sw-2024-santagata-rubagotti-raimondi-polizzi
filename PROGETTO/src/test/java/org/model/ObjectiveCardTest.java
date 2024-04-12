8a12ec9f08d8192e503ec42118204f45f26574ef
franc <francesco.raimondi3@mail.polimi.it>
2024-04-12 11:04:20 +0200

    Unified test in a single test class  (objective card)

--- PROGETTO/src/test/java/org/model/ObjectiveCardTest.java
+++ PROGETTO/src/test/java/org/model/ObjectiveCardTest.java
@@ -1,12 +1,27 @@
 package org.model;
 
 import junit.framework.TestCase;
+
 import java.util.HashMap;
 import java.util.Map;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ObjectiveCardNumberTest extends TestCase {
-    public void addPointsToPlayerTest() { //see the comments in Objective_card_number to comprehend how this method covers all the test cases
+public class ObjectiveCardTest extends TestCase {
+
+    public void testAddPointsToPlayer1() {
+        //let's use for example the blue diagonal pattern (down to up, left to right)
+        ObjectiveCard usedCard = new ObjectiveCard(2, 2, null, new Coordinates(1, 1), new Coordinates(2, 2), AngleType.INSECT, AngleType.INSECT, AngleType.INSECT);
+        Map<Coordinates, AngleType> playedCards;
+        playedCards = new HashMap<>();
+        playedCards.put(new Coordinates(1, 1), AngleType.INSECT);
+        playedCards.put(new Coordinates(2, 2), AngleType.INSECT);
+        playedCards.put(new Coordinates(3, 3), AngleType.INSECT);
+        playedCards.put(new Coordinates(4, 4), AngleType.INSECT);
+        playedCards.put(new Coordinates(5, 5), AngleType.INSECT);
+        playedCards.put(new Coordinates(6, 6), AngleType.FUNGI);  //see the drive file ObjectiveCardPatternTest
+        assertEquals(usedCard.addPointsToPlayer(new Player()), 2); //the algorithm is correct if it gives 2 points to the player
+    }
+
+    public void testAddPointsToPlayer2(){
         Map<CountingType, Integer> resources = new HashMap<>(); //the structure tha memorizes the resources needed and that is contained in Objective_card_number
         resources.put(CountingType.JAR,1);
         resources.put(CountingType.FEATHER,1);
@@ -16,7 +31,6 @@ public class ObjectiveCardNumberTest extends TestCase {
         numResources.put(AngleType.JAR, 3); //that means that in the Table there are three visible jars
         numResources.put(AngleType.FEATHER, 2);
         numResources.put(AngleType.SCROLL, 1); //this is the lowest number (if compared to the two lines above)
-
         assertEquals(usedCard.addPointsToPlayer(new Player()),3); //three points because we have only one set of: jar, feather, scroll
+        }
     }
-}
