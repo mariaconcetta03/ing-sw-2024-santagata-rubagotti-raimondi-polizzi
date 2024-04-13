457761de292a9ae9f01a91b62366e5e8c64ffda4
Andrea <andrea.rubagotti2002@gmail.com>
2024-04-13 11:10:21 +0200

    Rollback applied to Board

--- PROGETTO/src/main/java/org/model/Board.java
+++ PROGETTO/src/main/java/org/model/Board.java
@@ -1,7 +1,5 @@
 package org.model;
-
 import java.util.*;
-
 public class Board {
     private static final int numCarte=80;
     private Set<Coordinates> playablePositions;
@@ -9,11 +7,9 @@ public class Board {
     private Map<Coordinates, AngleType> playedCards; //we need that for the objective_pattern
     private int boardDimensions;
     private Map<AngleType, Integer> numResources;
-
     //this will be the board of each player
     private PlayableCard table[][];
     private Player player;
-
     //constructor of Board that takes the number of player that are playing the game
     public Board(Player player){
         //to round up the result
@@ -36,7 +32,6 @@ public class Board {
         numResources.put(AngleType.NO_RESOURCE,0);
         numResources.put(AngleType.ABSENT,0);
     }
-
     /**
      * This method creates and initialises the table of the specific Player, after the number of players is set
      * @param nPlayers is the number of players that are playing the Game
@@ -50,37 +45,35 @@ public class Board {
             }
         }
     }
-
     /**
      * This method places the first card for a player in the middle of the table (boardDimension/2) and adds related resources
      * @param baseCard is the base card the Player has placed
      */
-    public void placeBaseCard(PlayableCard baseCard) {
-        this.table[boardDimensions / 2][boardDimensions / 2] = baseCard;
-        baseCard.setPosition(new Coordinates(boardDimensions / 2, boardDimensions / 2));
+    public void placeBaseCard(PlayableCard baseCard){
+        this.table[boardDimensions/2][boardDimensions/2] = baseCard;
 
         //adding the new card resources
         //if played on the front (only 4 angles)
 
-        if (baseCard.getOrientation()) {
+        if(baseCard.getOrientation()) {
             numResources.put(baseCard.get_front_down_left(), numResources.get(baseCard.get_front_down_left()) + 1);
             numResources.put(baseCard.get_front_down_right(), numResources.get(baseCard.get_front_down_right()) + 1);
             numResources.put(baseCard.get_front_up_left(), numResources.get(baseCard.get_front_up_left()) + 1);
             numResources.put(baseCard.get_front_up_right(), numResources.get(baseCard.get_front_up_right()) + 1);
-        } else {
+        }else{
             //if played on the back side (4 angles and central resources
 
-            for (AngleType t : baseCard.getCentralResources()) {
-                numResources.put(t, numResources.get(t) + 1);
+            for(AngleType t : baseCard.getCentralResources()){
+                numResources.put(t, numResources.get(t)+1);
             }
-                numResources.put(baseCard.get_back_down_left(), numResources.get(baseCard.get_back_down_left()) + 1);
-                numResources.put(baseCard.get_back_down_right(), numResources.get(baseCard.get_back_down_right()) + 1);
-                numResources.put(baseCard.get_back_up_left(), numResources.get(baseCard.get_back_up_left()) + 1);
-                numResources.put(baseCard.get_back_up_right(), numResources.get(baseCard.get_back_up_right()) + 1);
-            }
-            updateUnplayablePositions(baseCard);
-            updatePlayablePositions(baseCard);
+            numResources.put(baseCard.get_back_down_left(), numResources.get(baseCard.get_back_down_left()) + 1);
+            numResources.put(baseCard.get_back_down_right(), numResources.get(baseCard.get_back_down_right()) + 1);
+            numResources.put(baseCard.get_back_up_left(), numResources.get(baseCard.get_back_up_left()) + 1);
+            numResources.put(baseCard.get_back_up_right(), numResources.get(baseCard.get_back_up_right()) + 1);
         }
+        updateUnplayablePositions(baseCard);
+        updatePlayablePositions(baseCard);
+    }
 
     /**
      * Places a card in the table and updates playable/unplayable positions
@@ -91,40 +84,26 @@ public class Board {
     //we have to add the requirements in the gold cards
     public boolean placeCard(PlayableCard card, Coordinates position) {
         int coveredAngles = 0;
-        boolean present = false;
-
-        Iterator<Coordinates> pos = playablePositions.iterator();
-
-        while(pos.hasNext()){
-            if (pos.next().equals(position)) {
-                present = true;
-            }
-        }
-
-        if (present && enoughResources(card)) {
+        if (playablePositions.contains(position) && enoughResources(card)) {
             //all the angles of the adjacent cards we could cover with the one we are placing
             AngleType upLeft=null;
             AngleType upRight=null;
             AngleType downLeft=null;
             AngleType downRight=null;
-
             //inserting the card
             card.setPosition(position);
             this.table[position.getX()][position.getY()] = card;
             playedCards.put(position, card.getCentralResources().get(0)); //only the base card (here not considered) has more than one resource in the center
-
             //adding the new card resources
             if(card.getOrientation()) {
                 numResources.put(card.get_front_down_left(), numResources.get(card.get_front_down_left())+1);
                 numResources.put(card.get_front_down_right(), numResources.get(card.get_front_down_right())+1);
                 numResources.put(card.get_front_up_left(), numResources.get(card.get_front_up_left())+1);
                 numResources.put(card.get_front_up_right(), numResources.get(card.get_front_up_right())+1);
-
             }else{
                 //adds the back resource if played on the back side
                 numResources.put(card.getCentralResources().get(0),numResources.get(card.getCentralResources().get(0))+1);
             }
-
             PlayableCard temp;
             //checks adjacent card's angles we might be covering with the new card (checking also the side where
             //the card temp we are checking is played)
@@ -152,15 +131,14 @@ public class Board {
                     downLeft = temp.get_back_up_right();
                 }
             }
-                if (table[position.getX() + 1][position.getY() - 1] != null) {
-                    temp = table[position.getX() + 1][position.getY() - 1];
-                    if (temp.getOrientation()) {
-                        downRight = temp.get_front_up_left();
-                    } else {
-                        downRight = temp.get_back_up_left();
-                    }
+            if (table[position.getX() + 1][position.getY() - 1] != null) {
+                temp = table[position.getX() + 1][position.getY() - 1];
+                if (temp.getOrientation()) {
+                    downRight = temp.get_front_up_left();
+                } else {
+                    downRight = temp.get_back_up_left();
                 }
-
+            }
             //subtracting the resource we are loosing when placing the new card
             if(upLeft!=null){
                 numResources.put(upLeft, numResources.get(upLeft)-1);
@@ -178,7 +156,6 @@ public class Board {
                 numResources.put(downRight, numResources.get(downRight)-1);
                 coveredAngles++;
             }
-
             //updating the playable and unplayable positions
             updateUnplayablePositions(card);
             updatePlayablePositions(card);
@@ -188,7 +165,6 @@ public class Board {
             return false;
         }
     }
-
     /**
      * When passed a resourceCard, the method gives the points scored by that specific card
      * When passed a goldCard, the method checks the "scoring condition" (angles coverage/number of objects)
@@ -196,20 +172,19 @@ public class Board {
      * @param coveredAngles is the number of angles covered by the PlayableCard just placed
      * @return points are the points actually scored thanks to card
      */
-    public int cardPoints (PlayableCard card, int coveredAngles) {
+    public int cardPoints (PlayableCard card, int coveredAngles){
         int points = card.getPoints(); // if the card gives you points in any case
-            if(card.isCoverAngleToReceivePoints()) {
-                points = coveredAngles * points;
-            } else if (card.isJarToReceivePoints()){
-                points = points * numResources.get(AngleType.JAR);
-            } else if (card.isFeatherToReceivePoints()){
-                points = points * numResources.get(AngleType.FEATHER);
-            } else if (card.isScrollToReceivePoints()) {
-                points = points * numResources.get(AngleType.SCROLL);
-            }
-            return points;
+        if(card.isCoverAngleToReceivePoints()) {
+            points = coveredAngles * points;
+        } else if (card.isJarToReceivePoints()){
+            points = points * numResources.get(AngleType.JAR);
+        } else if (card.isFeatherToReceivePoints()){
+            points = points * numResources.get(AngleType.FEATHER);
+        } else if (card.isScrollToReceivePoints()) {
+            points = points * numResources.get(AngleType.SCROLL);
+        }
+        return points;
     }
-
     /**
      * This method checks if the Player has enough resources to place the card
      * @param card is the card the Player has just placed
@@ -227,8 +202,6 @@ public class Board {
             return true;
         }
     }
-
-
     /**
      * Adding all the positions where the angle is ABSENT (players can't use that angle)
      * @param card
@@ -263,7 +236,6 @@ public class Board {
             }
         }
     }
-
     /**
      * Adding all the positions where the player can place a card in the future
      * @param card
@@ -303,7 +275,6 @@ public class Board {
             }
         }
     }
-
     /**
      * Getter method
      * @return a Set containing the positions where it's possible to play a card
@@ -311,7 +282,6 @@ public class Board {
     public Set<Coordinates> getPlayablePositions() {
         return playablePositions;
     }
-
     /**
      * Getter method
      * @return
@@ -319,7 +289,6 @@ public class Board {
     public Set<Coordinates> getUnPlayablePositions() {
         return unPlayablePositions;
     }
-
     /**
      * Getter method
      * @return
@@ -327,7 +296,6 @@ public class Board {
     public int getBoardDimensions() {
         return boardDimensions;
     }
-
     /**
      * Getter method
      * @return
@@ -335,7 +303,6 @@ public class Board {
     public Map<AngleType, Integer> getNumResources() {
         return numResources;
     }
-
     /**
      * Getter method
      * @return
@@ -343,7 +310,6 @@ public class Board {
     public PlayableCard[][] getTable() {
         return table;
     }
-
     /**
      * Getter method
      * @return a Map containing all the cards played by the Player with their Coordinates
