db81672c23e86391313719c8de38c4b5ed9a8d41
Andrea <andrea.rubagotti2002@gmail.com>
2024-05-17 10:28:34 +0200

    Added new classes to the GUI.

--- PROGETTO/src/main/java/view/GUI/InterfaceGUI.java
+++ PROGETTO/src/main/java/view/GUI/InterfaceGUI.java
@@ -1,6 +1,8 @@
 package view.GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.*;
 
@@ -14,8 +16,13 @@ public class InterfaceGUI extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(InterfaceGUI.class.getResource("GUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
 
 
